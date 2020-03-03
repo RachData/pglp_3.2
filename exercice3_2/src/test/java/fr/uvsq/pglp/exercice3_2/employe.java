deb1b187068bc4c40099462d32dc4cd578557ca8
RachData <rachidderra@gmail.com>
2020-03-03 22:54:10 +0100

    creation du fichier employe et modification

---
+++ exercice3_2/src/test/java/fr/uvsq/pglp/exercice3_2/employe.java
@@ -0,0 +1,14 @@
+
+public class employe extends allEmploye{
+	final private int salaire=1500;
+	private int startyear;
+	public employe(int startyear) {
+		this.startyear=startyear;
+	}
+	@Override
+	public double salaire() {
+		// TODO Auto-generated method stub
+		return salaire+(2020-this.startyear);
+	}
+
+}
