f9184459db49b87dac9314af37dd7cf4f1d17539
RachData <rachidderra@gmail.com>
2020-03-04 00:08:39 +0100

    Ajout d'un manageur dans les tests

--- exercice3_2/src/test/java/fr/uvsq/pglp/exercice3_2/allEmployeTest.java
+++ exercice3_2/src/test/java/fr/uvsq/pglp/exercice3_2/allEmployeTest.java
@@ -14,15 +14,19 @@ public class allEmployeTest {
 		vendeur v2 = new vendeur(20);
 		employe e1=new employe(2010);
 		employe e2=new employe(2005);
+		Manager M1=new Manager(10);
+		Manager M2=new Manager(20);
 		ArrayList <allEmploye> listEmploye=new ArrayList<allEmploye>();
 		listEmploye.add(v1);
 		listEmploye.add(v2);
 		listEmploye.add(e2);
 		listEmploye.add(e1);
+		listEmploye.add(M1);
+		listEmploye.add(M2);
 		double salaireTotal=0;
 		for(int i=0;i < listEmploye.size();i++)
 			salaireTotal+=listEmploye.get(i).salaire();
-		assertTrue(salaireTotal==7055);
+		assertTrue(salaireTotal==13055);
 	}
 
 }
