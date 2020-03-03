1f5775dff240a7c75068f6f4a490549e470f62c7
RachData <rachidderra@gmail.com>
2020-03-03 23:42:32 +0100

    Creation du fichier test allEmploye

--- exercice3_2/src/test/java/fr/uvsq/pglp/exercice3_2/allEmployeTest.java
+++ exercice3_2/src/test/java/fr/uvsq/pglp/exercice3_2/allEmployeTest.java
@@ -2,13 +2,26 @@ package fr.uvsq.pglp.exercice3_2;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class allEmployeTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		vendeur v1 = new vendeur(10);
+		vendeur v2 = new vendeur(20);
+		employe e1=new employe(2010);
+		employe e2=new employe(2005);
+		ArrayList <allEmploye> listEmploye=new ArrayList<allEmploye>();
+		listEmploye.add(v1);
+		listEmploye.add(v2);
+		listEmploye.add(e2);
+		listEmploye.add(e1);
+		double salaireTotal=0;
+		for(int i=0;i < listEmploye.size();i++)
+			salaireTotal+=listEmploye.get(i).salaire();
 	}
 
 }
